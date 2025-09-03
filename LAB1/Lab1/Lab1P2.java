3b68b0f15cb77bc77bf400e0e626d87f6bc4e168
brunda222 <brundabandaru222@gmail.com>
2025-09-03 12:31:03 +0530

    Add files via upload

---
+++ LAB1/Lab1/Lab1P2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Lab1P2{
+        public static void main(String...args){
+         	var a = 20;
+         	var b = 50.5;
+   		var c = a+b;
+         	System.out.println(c);
+         }
+ }
