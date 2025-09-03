5d27ca7986db536c7ca61ccedf01039ccec582f2
brunda222 <brundabandaru222@gmail.com>
2025-09-03 11:32:37 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,57 @@
+class Lab1P1{				
+         public static void main(String...args){
+               Byte b = 120;
+                Byte bb = b;
+                
+                System.out.println(Byte.MAX_VALUE);
+                System.out.println(bb.MIN_VALUE);
+                System.out.println(bb.SIZE);
+                System.out.println(bb.TYPE);
+                
+               System.out.println("--------INTEGER---------");
+                 int y = 120;
+                Integer it = y;
+                
+                System.out.println(Integer.MAX_VALUE);
+                System.out.println(it.MIN_VALUE);
+                System.out.println(it.SIZE);
+                System.out.println(it.TYPE);
+                
+                System.out.println("--------FLOAT---------");
+                 float f = 120;
+                 Float ff = f;
+                
+                System.out.println(Float.MAX_VALUE);
+                System.out.println(ff.MIN_VALUE);
+                System.out.println(ff.SIZE);
+                System.out.println(ff.TYPE);
+                
+                System.out.println("--------SHORT---------");
+                 short s = 120;
+                 Short ss = s;
+                
+                System.out.println(Short.MAX_VALUE);
+                System.out.println(ss.MIN_VALUE);
+                System.out.println(ss.SIZE);
+                System.out.println(ss.TYPE);
+                
+                System.out.println("--------DOUBLE---------");
+                 double d = 120;
+                 Double dd = d;
+                
+                System.out.println(Double.MAX_VALUE);
+                System.out.println(dd.MIN_VALUE);
+                System.out.println(dd.SIZE);
+                System.out.println(dd.TYPE);
+                
+                System.out.println("--------BOOLEAN---------");
+                 boolean a = 120;
+                 Boolean aa = a;
+                
+                System.out.println(Boolean.MAX_VALUE);
+                System.out.println(aa.MIN_VALUE);
+                System.out.println(aa.SIZE);
+                System.out.println(aa.TYPE);
+                
+         }
+}
