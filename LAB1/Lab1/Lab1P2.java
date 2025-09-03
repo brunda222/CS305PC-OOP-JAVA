903453f72ea3dd4be626100c4d52b2827f495f58
brunda222 <brundabandaru222@gmail.com>
2025-09-03 12:36:49 +0530

    Delete LAB1/Lab1 directory

--- LAB1/Lab1/Lab1P2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class Lab1P2{
-        public static void main(String...args){
-         	var a = 20;
-         	var b = 50.5;
-   		var c = a+b;
-         	System.out.println(c);
-         }
- }
