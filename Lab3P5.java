efca488c8800fbcab6e350467fc6b23f938e12db
brunda222 <brundabandaru222@gmail.com>
2025-09-24 12:39:31 +0530

    Add files via upload

---
+++ Lab3P5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Lab3P51{
+    String name;
+        public static void main(String..args){
+            System.out.println("I am a method in Lab3P6');
+          }
+          public void demonstrate(String s){
+          this.name=s;
+          }
+          public string getName(){
+                return this.name;
+                }
